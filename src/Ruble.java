c70caa96f9bbf0dae85689ff1c198ee1305ea298
Жиза <grekovgoga@gmail.com>
2023-02-20 01:36:45 +0300

    new

--- src/Ruble.java
+++ src/Ruble.java
@@ -12,12 +12,12 @@ public class Ruble {
         int pl2 = am + 8;
         int pl3 = am + 7;
         int pl4 = am + 6;
-        boolean b1 = am == 1 || pl % 2 == 0;   // рубль
-        boolean b2 = pl2 % 2 == 0 || pl3 % 2 == 0 || pl4 % 2 == 0;  // рубля
+        boolean b1 = am == 1 || pl % 10 == 0;   // рубль
+        boolean b2 = pl2 % 10 == 0 || pl3 % 10 == 0 || pl4 % 10 == 0;  // рубля
         boolean b3 = am < 5 || am > 19;
         if (b1 == true && b3 == true && am % 2 != 0) {
             System.out.println(am + " " + ryb);
-        } else if (b2 == true && b3 == true && am % 2 != 0){
+        } else if (b2 == true && b3 == true || am == 2){
                 System.out.println(am + " " + ryb3);
             } else {
             System.out.println(am + " " + ryb2);
